9767d749a8ee15a71d224cd44ea00a19faaabf58
gabrielleon31 <bygama31@gmail.com>
2025-03-17 23:37:00 +0100

    Ocultacion de secretos

--- config/config.js
+++ config/config.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 module.exports = {
-  mongoURI: "mongodb+srv://admin:tOPjOb1jtyrUGJW3@productsapi.kgc0x.mongodb.net/?retryWrites=true&w=majority&appName=ProductsAPI"
-};
+  mongoURI: process.env.MONGOURI
+}
